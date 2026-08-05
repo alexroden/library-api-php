2de132fb7825ce8f6bb7bd89d1747036a6e6335f
alex <b.r_alex@hotmail.co.uk>
2026-08-05 20:14:29 +0100

    #13: add categories relationship to books

--- database/migrations/0012_create_book_categories_table.up.sql
+++ database/migrations/0012_create_book_categories_table.up.sql
@@ -1,18 +1,18 @@
-CREATE TABLE book_authors (
+CREATE TABLE book_categories (
    id INT AUTO_INCREMENT PRIMARY KEY,
    book_id INT NOT NULL,
-   author_id INT NOT NULL,
+   category_id INT NOT NULL,
    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-   CONSTRAINT fk_book_authors_book
+   CONSTRAINT fk_book_categories_book
        FOREIGN KEY (book_id) REFERENCES books(id)
            ON DELETE CASCADE,
 
-   CONSTRAINT fk_book_authors_author
-       FOREIGN KEY (author_id) REFERENCES authors(id)
+   CONSTRAINT fk_book_categories_category
+       FOREIGN KEY (category_id) REFERENCES categories(id)
            ON DELETE CASCADE,
 
-   CONSTRAINT uq_book_authors
-       UNIQUE (book_id, author_id)
+   CONSTRAINT uq_book_categories
+       UNIQUE (book_id, category_id)
 );
